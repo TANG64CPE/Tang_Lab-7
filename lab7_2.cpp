8008715c9071beece11732d0994e731dadcd6847
TANGCPE64 <natsuphat_th@cmu.ac.th>
2021-11-30 17:50:14 +0700

    non-Complete

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -2,13 +2,38 @@
 
 using namespace std;
 
-int main(){
-	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
+int main()
+{
+	int i = 0,N=10000, P[5000]={}, k, result;
+	P[i]=1;
+	while ( P[i] % 2 == 1)
+	{
+		return P[5000];
 		i++;
 	}
-	
+	cout << "Please input k:";
+	cin >> k;
+	if( k > 0)
+	{
+		if ( k < 5000)
+		{
+			if( P[i] % k == 0)
+			{
+			while(i < 5000)
+			{
+				P[i] = 2*i+1;
+				i++;
+			}
+			}else{
+				result = result + P[i];
+			}
+		}else
+		{
+			cout << result;
+		}
+	}else
+	{
+		cout << "Invalid Input!!!";
+	}
 	return 0;
 }
